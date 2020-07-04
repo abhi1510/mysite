e6df02ecd4ac9a34665a498a67bf12b065aa886a
Abhinav Kumar <abhik.chy@gmail.com>
2020-07-05 05:07:53 +0530

    adding database uri to env

--- app/config.py
+++ app/config.py
@@ -4,6 +4,6 @@ import os
 class Config:
     DEBUG = os.environ.get('DEBUG')
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'  # os.environ.get('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     CONTACT_SUBJECT = 'Message from your site.'
